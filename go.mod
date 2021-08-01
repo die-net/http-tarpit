fe0a3a47fb084a00f7fac2eda0c8677e8108f1b6
Aaron Hopkins <ahopkins@ziprecruiter.com>
2021-07-31 17:45:12 -0700

    Switch license from GPL 2 to Apache 2.  Switch from Travis to Github Actions.

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/die-net/http-tarpit
 
-go 1.12
+go 1.16
--- main.go
+++ main.go
@@ -11,15 +11,17 @@ import (
 	"github.com/die-net/http-tarpit/tarpit"
 )
 
-var listenAddr = flag.String("listen", ":8080", "The [IP]:port to listen for incoming connections on.")
-var workers = flag.Int("workers", runtime.NumCPU(), "The number of worker threads to execute.")
-var period = flag.Duration("period", 16*time.Second, "Time between each byte sent on a connection.")
-var timeslice = flag.Duration("timeslice", 50*time.Millisecond, "How often each thread should wake up to send.")
-var contentType = flag.String("content_type", "text/html", "The content-type to send with the response.")
-var minResponseLen = flag.Int64("min_response_len", 1048576, "The minimum number of bytes to send total per connection.")
-var maxResponseLen = flag.Int64("max_response_len", 10485760, "The maximum number of bytes to send total per connection.")
-var rcvBuf = flag.Int("rcvbuf", 2048, "Kernel receive buffer size (0=default).")
-var sndBuf = flag.Int("sndbuf", 2048, "Kernel send buffer size (0=default).")
+var (
+	listenAddr     = flag.String("listen", ":8080", "The [IP]:port to listen for incoming connections on.")
+	workers        = flag.Int("workers", runtime.NumCPU(), "The number of worker threads to execute.")
+	period         = flag.Duration("period", 16*time.Second, "Time between each byte sent on a connection.")
+	timeslice      = flag.Duration("timeslice", 50*time.Millisecond, "How often each thread should wake up to send.")
+	contentType    = flag.String("content_type", "text/html", "The content-type to send with the response.")
+	minResponseLen = flag.Int64("min_response_len", 1048576, "The minimum number of bytes to send total per connection.")
+	maxResponseLen = flag.Int64("max_response_len", 10485760, "The maximum number of bytes to send total per connection.")
+	rcvBuf         = flag.Int("rcvbuf", 2048, "Kernel receive buffer size (0=default).")
+	sndBuf         = flag.Int("sndbuf", 2048, "Kernel send buffer size (0=default).")
+)
 
 func main() {
 	flag.Parse()
